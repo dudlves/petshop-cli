89509964424cae53d9fd0258dd7edb7be24056cd
Maria Eduarda <dudlves17@gmail.com>
2021-10-04 16:52:44 -0300

    primeiro commit

---
+++ funcoes.js
@@ -0,0 +1,51 @@
+const cachorros = require('./database/cachorros.json');
+const fs = require('fs')
+
+module.exports = {
+    listar:function(){
+        console.table(cachorros)
+    },
+    descrever: function(pos){
+        if(pos >= cachorros.length || pos < 0){
+            console.error("Cachorro inexistente")
+            return
+        }
+
+        let c = cachorros[pos]
+        console.log(`Nome: ${c.nome}`)
+        console.log(`Sexo: ${c.sexo}`)
+        if(c.castrado){
+            console.log("Castrado: sim")
+        }else{
+            console.log("Castrado: não")
+        }
+        console.log(`Data de Nascimento: ${c.dataDeNascimento}`)
+        console.log(`Peso: ${c.peso}`)
+
+        console.log("Vacinas:")
+        console.table(c.vacinas)
+
+        console.log("Serviços:")
+        console.table(c.servicos)
+        
+    },
+    //adicionar
+    adicionar: function($nome, $sexo, $castrado, $dataDeNascimento, $peso){
+        //objeto cachorro
+        //adicionar o cachorro criado a array de cachorros 
+        //gravar array de cachorros no arquivo cachorros.json
+        let dog = {
+            nome: $nome,
+            sexo: $sexo,
+            castrado: $castrado,
+            dataDeNascimento: $dataDeNascimento,
+            peso: $peso,
+            vacinas: [],
+            servicos: []
+        }
+
+        cachorros.push(dog)
+        fs.writeFileSync('./database/cachorros.json',JSON.stringify(cachorros))
+    }
+    
+}
